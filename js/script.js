9efe4989c6be2ce35b7c34fb3be04fb81eafe1d3
BcodezMUJJE <billbrightmateg@gmail.com>
2023-11-25 07:09:38 -0800

    website

--- js/script.js
+++ js/script.js
@@ -22,6 +22,15 @@ window.onscroll = () =>{
 }
 
 
+window.onload = () =>{
+  if(window.scrollY > 0){
+    document.querySelector('.header').classList.add(active);
+  }else{
+    document.querySelector('.header').classList.remove(active);
+  }
+}
+
+
 
 // for home section
 
@@ -34,5 +43,81 @@ var swiper = new Swiper(".home-slider", {
   autoplay: {
     delay: 9500,
     disableOnInteraction: false,
+  },
+});
+
+// for features section
+
+var swiper = new Swiper(".feature-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  
+  breakpoints: {
+    0: {
+      slidesPerview: 1,
+    },
+    768: {
+      slidesPerview: 2,
+    },
+    991: {
+      slidesPerview: 1,
+    },
+  },
+});
+
+
+// for trainer section
+
+var swiper = new Swiper(".trainer-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  
+  breakpoints: {
+    0: {
+      slidesPerview: 1,
+    },
+    768: {
+      slidesPerview: 2,
+    },
+    991: {
+      slidesPerview: 1,
+    },
+  },
+});
+
+// for bolgs section
+
+var swiper = new Swiper(".blogs-slider", {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 8500,
+    disableOnInteraction: false,
+  },
+  
+  breakpoints: {
+    0: {
+      slidesPerview: 1,
+    },
+    768: {
+      slidesPerview: 2,
+    },
+    991: {
+      slidesPerview: 1,
+    },
   },
 });
